1b32f0b2be9e34e8e82663e5f3d8b98915d8c703
voya-voja <61328254+voya-voja@users.noreply.github.com>
2023-04-17 22:27:07 -0400

    dann5 version 2.2.0 mac adjustments

--- d5o/dllmain.cpp
+++ d5o/dllmain.cpp
@@ -644,7 +644,7 @@ PYBIND11_MODULE(d5o2, m) {
 			.def(py::self == py::self, "compares the contents")
 
 			.def("__setitem__", [](Bits& self, size_t index, bool val) { self[index] = val; }, "Sets a value at the position 'index'")
-			.def("__getitem__", [](Bits& self, size_t index) { return self[index]; }, "Returns a bit at the position 'index'")
+			.def("__getitem__", [](Bits& self, size_t index) { return &self[index]; }, "Returns a bit at the position 'index'")
 
 			.def("all", &Bits::all, "checks if all of the bits are set to true")
 			.def("any", &Bits::any, "checks if any of the bits are set to true")
