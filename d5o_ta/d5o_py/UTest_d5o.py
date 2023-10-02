fea4329e8a63a24f09e114090b26ff3c190f47f4
voya-voja <61328254+voya-voja@users.noreply.github.com>
2023-10-02 14:41:19 -0400

    prime and rsa functions

--- d5o_ta/d5o_py/UTest_d5o.py
+++ d5o_ta/d5o_py/UTest_d5o.py
@@ -11,15 +11,43 @@ from dann5.dwave import Solvers
 from dann5.d5o2 import Qwhole, Qbin, Qblock, Qanalyzer
 from PrimeNumberGenerator import PNGen
 
+def prime(p0, p1):
+    for k in range(p0):
+        for f in range(1, 100, 2):
+            p = f * p0 * p1 - 2 ** (k + 1)
+            print(p)
+            p = f * p0 * p1 + 2 ** (k + 1)
+            print(p)
+
+def rsa(primeP, primeQ, message):
+    publikkeyN = primeP * primeQ
+    baseD = (primeP-1) * (primeQ-1)
+    publickKeyE = 2
+    for publickKeyE in range(7, baseD):
+        if baseD % publickKeyE != 0:
+            break
+    publickKeyE = 7
+    secretD = int((baseD + 1) / publickKeyE)
+    encrypt = (message ** publickKeyE) % publikkeyN
+    decrypt = (encrypt ** secretD) % publikkeyN
+    print("N: {}, base-D: {}, e: {}, d: {}, encrypt: {}, decrypt: {}".format(publikkeyN, baseD, publickKeyE, secretD, encrypt, decrypt))
+    
+    
+    
+    
 def main():
+    rsa(11, 17, 99) # only works when public-key e is 7 for message in 2-186
+    rsa(3, 11, 23)   # works for mesage in 2-32 
+    rsa(13,19, 88)
+    #prime(3,5)
     #solvers = Solvers(1000)
 
     #test = UTestQwhole(solvers)
     #test.runAll(0)
-    
+"""    
     png = PNGen(4, False)
     png.prime6()
-"""
+
     solvers = Solvers(1000)
     for nQbits in range(2,22,2):
         x = Qwhole(nQbits, "x"); y = Qwhole(int(nQbits / 2), "y"); r = Qwhole("r", nQbits);
